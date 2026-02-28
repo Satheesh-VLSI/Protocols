ed801491dbd89a92e6cc7baadc4fc069ea38bfae
Satheesh <satheeshnarayan05@gmail.com>
2026-02-28 17:53:34 +0530

    Create 9)UART_TESTBENCH.v

---
+++ Off_Chip_Protocols/UART_Protocol/9)UART_TESTBENCH.v
@@ -0,0 +1,76 @@
+
+module uart_tb;
+
+reg t_clk;
+reg r_clk;
+reg rst;
+reg w_en;
+reg [7:0]TDR;
+
+wire busy;
+wire done;
+wire frame_err;
+wire parity_err;
+wire [7:0] dout;
+
+
+top DUT (.t_clk(t_clk),
+    .r_clk(r_clk),
+    .rst(rst),
+    .w_en(w_en),
+    .TDR(TDR),
+    .busy(busy),
+    .done(done),
+    .frame_err(frame_err),
+    .parity_err(parity_err),
+    .dout(dout)
+);
+
+
+always #20 t_clk=~t_clk;   // 50MHz
+
+
+always #10 r_clk=~r_clk;   //100MHZ
+
+initial begin
+    t_clk=0;
+    r_clk= 0;
+    rst=1;
+    w_en=0;
+    TDR=8'd0;
+
+    #200;
+    rst=0;
+
+   
+  repeat(10) @(posedge t_clk);
+
+   
+    TDR=8'd49;  
+    w_en=1;
+    @(posedge t_clk);
+    w_en=0;
+ 
+
+    // wait for Receiver to be done
+    wait(done==1);
+
+  $display("************************************");
+    $display("Sent= %d",TDR);
+    $display("Received=%d",dout);
+    $display("FrameErr=%b",frame_err);
+    $display("ParityErr=%b",parity_err);
+  $display("************************************");
+
+    #2000;
+    $finish;
+end
+
+initial begin
+    $dumpfile("uart.vcd");
+    $dumpvars(0, uart_tb);
+
+$monitor("t=%0t  busy=%b done=%b frame_err=%b parity_err=%b dout=%d",$time,busy,done,frame_err,parity_err,dout);
+end
+
+endmodule
