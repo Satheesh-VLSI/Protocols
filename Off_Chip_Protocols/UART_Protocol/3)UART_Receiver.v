aad897d165b39ca984e81c9cc32167bd3d89ad62
Satheesh <satheeshnarayan05@gmail.com>
2026-03-02 11:58:04 +0530

    Update 3)UART_Receiver.v

--- Off_Chip_Protocols/UART_Protocol/3)UART_Receiver.v
+++ Off_Chip_Protocols/UART_Protocol/3)UART_Receiver.v
@@ -1,40 +1,38 @@
 
 `include "o_parity_check.v"
 
-
 module UART_Receiver(clk,rst,rx_en,Rx,done,dout,frame_err,parity_err);
 input clk,rst,rx_en;
 output reg [7:0] dout;
-input Rx;
+input Rx; 
 output reg done,frame_err,parity_err;
 
 //Controller
 localparam IDLE=3'B000,
-        START=3'B001,
-        DATA=3'B010,
-        PARITY=3'B011,
-        STOP=3'B100,
-	    DONE_ERROR=3'B101;
+           START=3'B001,
+           DATA=3'B010,
+           PARITY=3'B011,
+           STOP=3'B100,
+	       DONE_ERROR=3'B101;
 
 reg [2:0] state,next_state;
 reg o_parity;
 wire o_parity_check;
 reg [7:0] TDR;
 reg [3:0] sample;
-wire  sh_en,sample_mid,sample_end;
-reg [2:0] counter;
-
+wire sample_mid,sample_end;
+ reg [2:0] index;
 
 assign sample_mid=(sample==4'd8)&&rx_en;
 assign sample_end=(sample==4'd15)&&rx_en;
 assign sh_en=((rx_en)&&(sample_mid)&&(state==DATA));
-  reg [2:0] index;
+ 
 
 
 //parity check
 Odd_Par_CHECK pc(.Data(TDR),
-	             .Odd_parity(o_parity),
-	             .OpCheck(o_parity_check));
+	.Odd_parity(o_parity),
+	.OpCheck(o_parity_check));
  //sequential logic
 always @(posedge clk or posedge rst)begin
         if(rst)
@@ -48,10 +46,10 @@ always @(*)begin
         next_state=state;
         case(state)
 		IDLE: next_state<=(Rx==0)? START:IDLE;
-        START: next_state<=(sample_end && Rx==0)?DATA:START;
-        DATA: next_state<=((sample_mid)&&(counter==3'd7))? PARITY:DATA;
-        PARITY: next_state<=(sample_end)? STOP:PARITY;
-        STOP:next_state<=(sample_end)? DONE_ERROR:STOP;
+          START: next_state<=(sample_end && Rx==0)?DATA:START;
+          DATA: next_state<=((sample_end)&&(index==3'd7))? PARITY:DATA;
+          PARITY: next_state<=(sample_end)? STOP:PARITY;
+          STOP:next_state<=(sample_end)? DONE_ERROR:STOP;
 		DONE_ERROR: next_state<=IDLE;
         endcase
 end
@@ -61,7 +59,6 @@ always @(posedge clk or posedge rst)begin
 	    sample<=0;
         frame_err<=0;
 	    parity_err<=0;
-        counter<=0;
 	    done<=0;
         index<=0;
     	dout<=0;
@@ -69,7 +66,7 @@ always @(posedge clk or posedge rst)begin
 	 
     end
     else begin
-       done<=0;
+      done<=0;
        if (state != next_state)
            sample <= 0;
        else if(rx_en && (state==START || state==DATA || state==PARITY || state==STOP))begin
@@ -85,40 +82,37 @@ always @(posedge clk or posedge rst)begin
             TDR<=0;  
             index<=0;
             sample <= 0;
-			counter<=0;
 			frame_err<=0;
 	        parity_err<=0;
 		    done<=0;
                 end
-          START:begin
+        START:begin
                 frame_err <= 0;
                 parity_err <= 0; 
-	        	counter<=0;
                 index<=0;
                 end
-         DATA:begin
-           if(sample_mid)begin 
+        DATA:begin
+           if(sample_mid)
              TDR[index]<=Rx;
-                 if(index < 3'd7)
-                    index <= index + 1;
-		     counter<=counter+1;
-                end
-           end
+         
+           if(sample_end)
+               index<=index+1;
+               
+         end
 		PARITY:begin
-		    if(sample_mid)	
+			if(sample_mid)	
                o_parity<=Rx;  
 		end
-        STOP:begin 
+        STOP:begin
+              if(sample_mid) 
 		        frame_err<=~Rx;
 		end
 		DONE_ERROR:begin
-            if(sample_mid)
-			     parity_err<=o_parity_check;
+          if(sample_mid)
+			parity_err<=o_parity_check;
 		    done<=1;
-			counter <= 0;
             dout<=TDR;
 		end
         endcase
     end
     endmodule
-
