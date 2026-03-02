16ad18eb0d259213997eb57a1b0510e64d8763ca
Satheesh <satheeshnarayan05@gmail.com>
2026-03-02 13:24:28 +0530

    Update and rename 3)UART_Receiver.v to 3_UART_Receiver.v

--- Off_Chip_Protocols/UART_Protocol/3_UART_Receiver.v
+++ Off_Chip_Protocols/UART_Protocol/3_UART_Receiver.v
@@ -1,6 +1,6 @@
 
 `include "o_parity_check.v"
-
+//UART Receiver with 8 Data bit - 1 Stop bit - 9600 baud rate for 100Mhz
 module UART_Receiver(clk,rst,rx_en,Rx,done,dout,frame_err,parity_err);
 input clk,rst,rx_en;
 output reg [7:0] dout;
@@ -12,8 +12,7 @@ localparam IDLE=3'B000,
            START=3'B001,
            DATA=3'B010,
            PARITY=3'B011,
-           STOP=3'B100,
-	       DONE_ERROR=3'B101;
+           STOP=3'B100;
 
 reg [2:0] state,next_state;
 reg o_parity;
@@ -45,12 +44,15 @@ end
 always @(*)begin
         next_state=state;
         case(state)
-		IDLE: next_state<=(Rx==0)? START:IDLE;
-          START: next_state<=(sample_end && Rx==0)?DATA:START;
+		  IDLE: next_state<=(Rx==0)? START:IDLE;
+          START:begin  if (sample_mid && Rx==1)
+                            next_state = IDLE;
+                       else
+                            next_state<=(sample_end)?DATA:START; 
+                 end
           DATA: next_state<=((sample_end)&&(index==3'd7))? PARITY:DATA;
           PARITY: next_state<=(sample_end)? STOP:PARITY;
-          STOP:next_state<=(sample_end)? DONE_ERROR:STOP;
-		DONE_ERROR: next_state<=IDLE;
+          STOP:next_state<=(sample_end)?IDLE:STOP;
         endcase
 end
 //output logic upon states
@@ -86,33 +88,35 @@ always @(posedge clk or posedge rst)begin
 	        parity_err<=0;
 		    done<=0;
                 end
-        START:begin
+			
+          START:begin
                 frame_err <= 0;
                 parity_err <= 0; 
                 index<=0;
                 end
-        DATA:begin
+			
+         DATA:begin
            if(sample_mid)
              TDR[index]<=Rx;
-         
            if(sample_end)
-               index<=index+1;
-               
+               index<=index+1;    
          end
+			
 		PARITY:begin
 			if(sample_mid)	
                o_parity<=Rx;  
 		end
+			
         STOP:begin
-              if(sample_mid) 
+          if(sample_mid)begin 
 		        frame_err<=~Rx;
+                parity_err<=o_parity_check;
+                dout<=TDR;
+		      end
+          if(sample_end)
+             done<=1;
 		end
-		DONE_ERROR:begin
-          if(sample_mid)
-			parity_err<=o_parity_check;
-		    done<=1;
-            dout<=TDR;
-		end
+			
         endcase
     end
-    endmodule
+endmodule
