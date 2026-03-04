9e3070a669da94147310e0ebf9638a7045765ec7
Satheesh <satheeshnarayan05@gmail.com>
2026-03-04 10:14:53 +0530

    Create 4_Transmitter_Baud_Gen.v

---
+++ Off_Chip_Protocols/UART_Protocol/UART_Parameterized_BaudRate/4_Transmitter_Baud_Gen.v
@@ -0,0 +1,26 @@
+
+module transmitter_baud_gen #(parameter baudrate=115200,t_frq=50000000)(clk,rst,tx_en);
+input clk,rst;
+output reg tx_en;
+
+reg [$clog2(t_frq/baudrate):0]count;
+
+always @(posedge clk or posedge rst)begin
+
+	if(rst)begin
+		tx_en<=0;
+		count<=0;
+	end
+
+  else if(count==t_frq/baudrate)begin  //count
+		tx_en<=1;
+	        count<=0;
+	end
+	else begin
+		count<=count+1;
+	        tx_en<=0;
+	end
+
+        end 
+endmodule
+
