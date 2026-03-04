941c5eeba9bab962a908f308e882e6647b48f86d
Satheesh <satheeshnarayan05@gmail.com>
2026-03-04 10:18:54 +0530

    Update 1_Top.v

--- Off_Chip_Protocols/UART_Protocol/UART_Parameterized_BaudRate/1_Top.v
+++ Off_Chip_Protocols/UART_Protocol/UART_Parameterized_BaudRate/1_Top.v
@@ -1,3 +1,8 @@
+`include "2_UART_Transmitter.v"
+`include "3_UART_Receiver.v"
+`include "4_Transmitter_Baud_Gen.v"
+`include "5_Receiver_Baud_Gen.v"
+
 module top #(parameter baudrate=115200,t_frq=50000000,r_frq=100000000) (
 input t_clk,r_clk,rst,w_en,
 input [7:0] TDR,
