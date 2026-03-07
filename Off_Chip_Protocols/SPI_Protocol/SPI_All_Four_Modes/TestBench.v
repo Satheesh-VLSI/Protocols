c73075bf7ebcd7259cf4e5298af897b7e284c704
Satheesh <satheeshnarayan05@gmail.com>
2026-03-07 18:13:32 +0530

    Update TestBench.v

--- Off_Chip_Protocols/SPI_Protocol/SPI_All_Four_Modes/TestBench.v
+++ Off_Chip_Protocols/SPI_Protocol/SPI_All_Four_Modes/TestBench.v
@@ -7,8 +7,8 @@ module tb_top #(parameter MODE=2'b11);
     wire m_busy, m_done, s_busy, s_done;
     
     // Clock generation
-    always #10 m_clk = ~m_clk;  // 50MHz
-    always #5  s_clk = ~s_clk;  // 100MHz
+    always #10 m_clk=~m_clk;  // 50MHz
+    always #5  s_clk=~s_clk;  // 100MHz
     
   top #(MODE) dut (
         .m_clk(m_clk),
@@ -32,27 +32,27 @@ module tb_top #(parameter MODE=2'b11);
         
         // Test Case 1
         rst = 1; #20 rst = 0;
-        #50 m_data_in =  165; s_data_in = 90;   start_en = 1;
+        #50 m_data_in=165; s_data_in=90;   start_en=1;
         wait(m_done==1); wait(s_done==1);
         $display("\n\n*****Multiple Data - SIngle Start Enable TEST SERIES*****");
         $display("Test 1 - Master RX: %d (should be 90)", m_data_out);
         $display("Test 1 - Slave RX:  %d (should be 165)", s_data_out);
         
         // Test  2  
-        m_data_in = 16;  s_data_in = 100;
+        m_data_in=16;  s_data_in=100;
         #40 wait(m_done==1); wait(s_done==1);
         
         $display("Test 2 - Master RX: %d (should be 100)", m_data_out);
         $display("Test 2 - Slave RX:  %d (should be 16)", s_data_out);
         
         // Test  3
-        #40 m_data_in = 33;  s_data_in = 66;  
+        #40 m_data_in=33;  s_data_in=66;  
         wait(m_done==1); wait(s_done==1);
         $display("Test 3 - Master RX: %d (should be 66)", m_data_out);
         $display("Test 3 - Slave RX:  %d (should be 33)", s_data_out);
         
         // Test  4
-        #40 m_data_in = 27; s_data_in = 0; #50 start_en=0;  
+        #40 m_data_in=27; s_data_in=0; #50 start_en=0;  
         wait(m_done==1); wait(s_done==1);
         $display("Test 4 - Master RX: %d (should be 0)", m_data_out);
       $display("Test 4 - Slave RX:  %d (should be 27)", s_data_out);
