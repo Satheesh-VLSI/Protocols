b0364e7046ec284f813a4536988e49fd1b22f190
Satheesh <satheeshnarayan05@gmail.com>
2026-03-07 17:25:56 +0530

    Update TestBench.v

--- Off_Chip_Protocols/SPI_Protocol/SPI_All_Four_Modes/TestBench.v
+++ Off_Chip_Protocols/SPI_Protocol/SPI_All_Four_Modes/TestBench.v
@@ -37,27 +37,27 @@ module tb_top #(parameter MODE=2'b11);
         $display("Test 1 - Master RX: %d (should be 90)", m_data_out);
         $display("Test 1 - Slave RX:  %d (should be 165)", s_data_out);
         
-        // Test Case 2  
+        // Test  2  
         m_data_in = 16;  s_data_in = 100;
         #40 wait(m_done==1); wait(s_done==1);
         
         $display("Test 2 - Master RX: %d (should be 100)", m_data_out);
         $display("Test 2 - Slave RX:  %d (should be 16)", s_data_out);
         
-        // Test Case 3
+        // Test  3
         #40 m_data_in = 33;  s_data_in = 66;  
         wait(m_done==1); wait(s_done==1);
         $display("Test 3 - Master RX: %d (should be 66)", m_data_out);
         $display("Test 3 - Slave RX:  %d (should be 33)", s_data_out);
         
-        // Test Case 4
+        // Test  4
         #40 m_data_in = 27; s_data_in = 0; #50 start_en=0;  
         wait(m_done==1); wait(s_done==1);
         $display("Test 4 - Master RX: %d (should be 0)", m_data_out);
       $display("Test 4 - Slave RX:  %d (should be 27)", s_data_out);
         
         $display("=== TESTS COMPLETE ===");
-      
+        // Test Case 1 
         #50 m_data_in=55; s_data_in=9; 
         start_en=1;#50 start_en=0;
         wait(m_done==1); wait(s_done==1);
@@ -65,21 +65,23 @@ module tb_top #(parameter MODE=2'b11);
         $display("Test 1 - Master RX: %d (should be 9)", m_data_out);
         $display("Test 1 - Slave RX:  %d (should be 55)", s_data_out);
         
-        // Test Case 2  
+        // Test  2  
         #50 m_data_in=123;  s_data_in = 108;  
         start_en=1; #50 start_en = 0;
         wait(m_done==1); wait(s_done==1);
         $display("Test 2 - Master RX: %d (should be 108)", m_data_out);
         $display("Test 2 - Slave RX:  %d (should be 123)", s_data_out);
         
-        // Test Case 3
+        // Test  3
         #50 m_data_in=42;  s_data_in=89;   
         start_en=1; #50 start_en=0;
         wait(m_done==1); wait(s_done==1);
         $display("Test 3 - Master RX: %d (should be 89)", m_data_out);
         $display("Test 3 - Slave RX:  %d (should be 42)", s_data_out);
         wait(m_done==0); wait(s_done==0);
-        // Test Case 4
+
+        
+        // Test  4
         m_data_in=200; s_data_in=0;
         start_en=1; #50 start_en= 0;
         wait(m_done==1); wait(s_done==1);
